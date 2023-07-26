cca108233a5fa169b97e50fede4828680c87c733
SnowDevLC <canalesluis9@gmail.com>
2023-07-26 20:40:58 -0300

    DEPLOY

--- Countries-APP/client/src/utils/const.js
+++ Countries-APP/client/src/utils/const.js
@@ -1,1 +1,2 @@
-export const URL_BASE = "http://localhost:3001";
+// export const URL_BASE = "http://localhost:3001";
+export const URL_BASE = "https://countries-back-2qeudt8rj-snowdevlc.vercel.app";
